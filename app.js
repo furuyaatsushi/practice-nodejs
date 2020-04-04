b74248fb2c1166bc711ebb38acf90807c7cc169d
furuyaatsushi <sdvxmnn@gmail.com>
2020-04-05 01:58:00 +0900

    create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+const express = require("express");
+const app = express();
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+const server = app.listen(3000, function(){
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
