e914fa1b0fd24d923a2c266512cc90d3e4d654a6
furuyaatsushi <sdvxmnn@gmail.com>
2020-04-05 04:20:17 +0900

    node server

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ const app = express();
 const server = app.listen(3000, function(){
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
+
+app.get('/', (req, res) => res.send('Hello World!'));
