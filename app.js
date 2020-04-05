da602eeab5d52b58e5ea3bab530e43e95ea16971
furuyaatsushi <sdvxmnn@gmail.com>
2020-04-05 19:58:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
-const express = require("express");
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-const server = app.listen(3000, function(){
-  console.log("Node.js is listening to PORT:" + server.address().port);
-});
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const options = { 
+  useUnifiedTopology : true,
+  useNewUrlParser : true
+}
+
+mongoose.connect('mongodb://127.0.0.1/UserData',options);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'DB connection error:'));
+db.once('open', () => console.log('DB connection successful'));
