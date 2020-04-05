9cc84f9aac34781a15c9ddd4cdad5c25d69ffcaa
furuyaatsushi <sdvxmnn@gmail.com>
2020-04-05 22:45:37 +0900

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
+router.use(express.json());
+
 router.get('/', async (req, res) => {
   console.log("user finding...");
 	const users = await User.find({});
 
 	res.json(users);
+
+});
+
+router.post('/', async (req,res)=>{
+	const user = new User({
+		name: req.body.name,
+		age: req.body.age
+	});
+
+	const savedUser = await user.save();
+	res.json(savedUser);
 
 });
 
