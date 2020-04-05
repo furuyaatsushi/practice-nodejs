94ed143ada04af646eeaefa01834bb6883a96207
furuyaatsushi <sdvxmnn@gmail.com>
2020-04-05 21:18:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const User = require('./models/User');
 
 const port = 3000;
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 const options = { 
   useUnifiedTopology : true,
   useNewUrlParser : true
