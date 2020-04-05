c443c2f57ce000b294f940c67821027df37aa668
furuyaatsushi <sdvxmnn@gmail.com>
2020-04-05 20:50:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const User = require('./models/User');
+
 const port = 3000;
 
 const options = { 
@@ -15,3 +17,11 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'DB connection error:'));
 db.once('open', () => console.log('DB connection successful'));
+
+app.get('/user', async (req, res) => {
+
+	const users = await User.find({});
+
+	res.json(users);
+
+});
