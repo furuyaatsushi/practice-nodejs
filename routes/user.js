680f225799a2bd4019d6a73bae2ff64282b218fc
furuyaatsushi <sdvxmnn@gmail.com>
2020-05-23 01:39:17 +0900

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,18 @@ const read = require('../read');
 const write = require('../write');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/unit/get', async (req, res) => {
+  const userId = req.query.userid;
+	const user = await read.readAsync(userId);
 
-	const users = await User.find({});
-
-	res.json(users);
+	res.json(user.unit);
 
 });
 
 router.post('/login', async (req,res)=>{
+  console.log(req.body.userId);
   const userId = req.body.userId;
   let user = await read.readAsync(userId);
-  console.log(user);
   if(!user){
     await write.writeAsync(userId);
     user = await read.readAsync(userId);
